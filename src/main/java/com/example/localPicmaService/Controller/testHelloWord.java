debd4395fab9c5895f57d5291d5f59d5411a26dc
SSLingFeng <582390473@qq,cin>
2025-07-02 13:31:27 +0800

    mastre

---
+++ src/main/java/com/example/localPicmaService/Controller/testHelloWord.java
@@ -0,0 +1,19 @@
+package com.example.localPicmaService.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testHelloWord {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/request")
+    public String request() {
+        return "psotResStr";
+    }
+}
