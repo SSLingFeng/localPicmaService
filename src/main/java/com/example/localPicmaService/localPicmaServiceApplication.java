b3c3974ff3cc2e5330d5ad8613ad858442b4ffff
灵风 <582390473@qq.com>
2025-07-16 14:31:40 +0800

    新增同步类，数据库工具类BUG修复

--- src/main/java/com/example/localPicmaService/localPicmaServiceApplication.java
+++ src/main/java/com/example/localPicmaService/localPicmaServiceApplication.java
@@ -1,6 +1,5 @@
 package com.example.localPicmaService;
 
-import com.example.localPicmaService.base.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +9,6 @@ public class localPicmaServiceApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(localPicmaServiceApplication.class, args);
-        test.main(null);
     }
 
 }
