12cb5f633265c6df45280f97835aca2fbaca1b40
灵风 <582390473@qq.com>
2025-07-16 11:19:47 +0800

    删除无用程序

--- src/main/java/com/example/localPicmaService/base/test.java
+++
@@ -1,9 +0,0 @@
-package com.example.localPicmaService.base;
-
-public class test {
-    public static void main(String[] args) {
-        String sql = "select * from frp_config_list";
-        Object s = DataSourceControl.runQuery(sql);
-        System.out.println(s);
-    }
-}
